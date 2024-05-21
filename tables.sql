45330d0c64ec391ab98c50822f0aa3df64e4f84d
Hossein-29 <hb3238816@gmail.com>
2024-05-21 17:55:35 +0330

    Add tables.sql for creating tables

---
+++ tables.sql
@@ -0,0 +1,91 @@
+-- command to run this on your local database
+-- cd directory_of_this_file
+-- psql -d database_name < tables.sql
+
+
+DROP TABLE IF EXISTS admins;
+CREATE TABLE admins(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    username VARCHAR(50),
+    access_level INTEGER
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    province VARCHAR(50),
+    city VARCHAR(50),
+    email VARCHAR(50),
+    phone VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS wallets;
+CREATE TABLE wallets(
+    id SERIAL PRIMARY KEY,
+    amount INT DEFAULT 0,
+    FOREIGN KEY(id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS payments;
+CREATE TABLE payments(
+    id SERIAL PRIMARY KEY,
+    amount INTEGER,
+    user_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES users(id)
+)
+
+DROP TABLE IF EXISTS categories;
+CREATE TABLE categories(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    parent_id INTEGER,
+    FOREIGN KEY(parent_id) REFERENCES categories(id) ON DELETE SET NULL
+);
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    category_id INT,
+    city VARCHAR(50),
+    province VARCHAR(50),
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    description VARCHAR(100),
+    state VARCHAR(50)
+);
+
+DROP TABLE IF EXISTS comments;
+CREATE TABLE comments(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    sent_at TIMESTAMPTZ DEFAULT NOW(),
+    text VARCHAR(200) NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE SET NULL,
+    FOREIGN KEY(product_id) REFERENCES products(id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS badges;
+CREATE TABLE badges(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    weight INT NOT NULL,
+    price INTEGER NOT NULL
+);
+
+DROP TABLE IF EXISTS contains_badge;
+CREATE TABLE contains_badge(
+    badge_id INT,
+    product_id INT,
+    FOREIGN KEY(badge_id) REFERENCES badges(id) ON DELETE CASCADE,
+    FOREIGN KEY(product_id) REFERENCES products(id) ON DELETE CASCADE
+);
+
+
+
+
+

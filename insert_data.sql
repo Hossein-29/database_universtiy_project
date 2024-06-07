1bad8b591fa7be5e92a1b75b1a779e8891e56046
Hossein-29 <hb3238816@gmail.com>
2024-06-07 22:12:46 +0330

    add insert tables file

---
+++ insert_data.sql
@@ -0,0 +1,47 @@
+COPY users
+FROM '/Users/hosseinb/projects/database_universtiy_project/divar_users.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY admins
+FROM '/Users/hosseinb/projects/database_universtiy_project/divar_admins.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY wallets
+FROM '/Users/hosseinb/projects/database_universtiy_project/divar_wallets.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY payments
+FROM '/Users/hosseinb/projects/database_universtiy_project/divar_payments.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY categories
+FROM '/Users/hosseinb/projects/database_universtiy_project/divar_categories.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY products
+FROM '/Users/hosseinb/projects/database_universtiy_project/divar_products.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY comments
+FROM '/Users/hosseinb/projects/database_universtiy_project/divar_comments.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY badges
+FROM '/Users/hosseinb/projects/database_universtiy_project/divar_bages.csv'
+DELIMITER ','
+CSV HEADER;
+
+COPY contains_badge
+FROM '/Users/hosseinb/projects/database_universtiy_project/divar_contains_badge.csv'
+DELIMITER ','
+CSV HEADER;
+
+
+
